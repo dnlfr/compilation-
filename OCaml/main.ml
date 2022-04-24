80cdb26d6eb683179247ebaabdd7be8bccac3410
dnlfr <danielwei.ferreira@gmail.com>
2022-04-24 19:08:12 +0200

    intro_ast

--- OCaml/main.ml
+++ OCaml/main.ml
@@ -17,10 +17,10 @@ let _ =                                          (*main en OCaml*)
 	       Printf.printf ("Ceci n'est pas une expression arithmetique\n")
 	else
 		try
-			let file = Sys.argv.(1)^".jsm" in (*On recupère le nom du fichier*)
+			let file = Sys.argv.(1) in (*On recupère le nom du fichier*)
 			let oc = open_out file in
 			let formatter_out = Format.formatter_of_out_channel oc in
-			let lexbuf = Lexing.from_channel (open_in Sys.argv.(1)) in (*lexeur lancé sur fichier*)
+			let lexbuf = Lexing.from_channel (open_in Sys.argv.(1)) in (*lexeur sur le fichier entré en argument*)
 	    while true do                                (*on ne s'arrête pas*)
         Parseur.main Lexeur.token lexbuf (*parseur une ligne*)
         |> Format.printf "%a\n%!" AST.print_AST ;
