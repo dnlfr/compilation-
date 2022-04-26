7d18ba22ba6874565246eb52ee3f95a3caba7634
dnlfr <danielwei.ferreira@gmail.com>
2022-04-26 16:08:40 +0200

    Modif

--- OCaml/AST.ml
+++ OCaml/AST.ml
@@ -4,7 +4,8 @@ type expression_a =
   | Mult  of expression_a * expression_a
   | Div   of expression_a * expression_a
   | Neg   of expression_a
-  | Num   of int
+  | Num   of int * float 
+  | Mod   of expression_a * int
 ;;
 
 
@@ -17,6 +18,7 @@ and print_AST form = let open Format in function
   | Moins (g,d) -> print_binaire form "Moins" g d
   | Mult  (g,d) -> print_binaire form "Mult" g d
   | Div   (g,d) -> print_binaire form "Div" g d
+  | Mod   (g,d) -> print_binaire form "Mod" g d
   | Neg    e    -> fprintf form "@[<2>%s@ %a@]" "Neg" print_AST e 
   | Num    n    -> fprintf form "@[<2>%s@ %i@]" "Num" n
 ;; 
@@ -27,5 +29,6 @@ and code form = let open Format in function
   | Num n -> fprintf "@[<2>%s@ %f@ %s@]" "CsteNb" n "\n"
   | Plus  (g, d) -> print_post_fixe form "AddiNb" g d 
   | Mult  (g, d) -> print_post_fixe form "MultNb" g d 
-  | Moins (g, d) -> print_post_fixe form "SubiNb"
+  | Moins (g, d) -> print_post_fixe form "SubiNb" g d
+  | Mod (g,d) -> print_post_fixe form "Mod" g d 
   | Neg   n ->  fprintf form "@[<2>%s@ %f@ %s@]" n "NegaNb\n"
