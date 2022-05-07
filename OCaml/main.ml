00e606d7970adda1bb941b670c83b73c2dc3ac66
Daniel Ferreira <danielferreira@Daniel.local>
2022-05-07 21:56:20 +0200

    Ajout des calculs des offsets pour la taille des expressions/cmd/prog

--- OCaml/main.ml
+++ OCaml/main.ml
@@ -8,7 +8,8 @@ let _ =                                          (*main en OCaml*)
 	    	let lexbuf = Lexing.from_channel stdin in    (*lexeur lancé sur stdin*)
 		    while true do                                (*on ne s'arrête pas*)
           Parseur.commande Lexeur.token lexbuf (*parseur une ligne*)
-          |> Format.printf "%a\n%!" AST.print_AST_cmd ;
+					|> AST.caloffset_cmd (* Calcul des offsets *)
+          |> Format.printf "%a\n%!" AST.code_cmd ;
 		    done
 	  with
 	   | Lexeur.Eof          -> exit 0							 (*impossible*)
@@ -17,12 +18,13 @@ let _ =                                          (*main en OCaml*)
 	       Printf.printf ("Ceci n'est pas une expression arithmetique\n")
 	else
 		try
-			let file = Sys.argv.(1)^".jsm" in (*On recupère le nom du fichier*)
+			let file = Sys.argv.(1)^".jsm" in (* Nom du fichier*)
 			let oc = open_out file in
 			let formatter_out = Format.formatter_of_out_channel oc in
 			let lexbuf = Lexing.from_channel (open_in Sys.argv.(1)) in (*lexeur sur le fichier entré en argument*)
 	    while true do                                (*on ne s'arrête pas*)
         Parseur.commande Lexeur.token lexbuf (*parseur une ligne*)
+				|> AST.caloffset_cmd (* Calcul des offsets *)
         |> Format.printf "%a\n%!" AST.code_cmd	 ;
 	    done
 		  with
